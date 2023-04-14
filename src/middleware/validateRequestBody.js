cf96788cf91d1a4a4ac9228a8a1ce9be91ff9ca5
Alan Eicker <alaneicker@gmail.com>
2023-04-14 15:18:11 -0500

    refactors validateRequestBody

--- src/middleware/validateRequestBody.js
+++ src/middleware/validateRequestBody.js
@@ -1,4 +1,5 @@
 import { Validator } from 'jsonschema';
+import { setErrorResponse } from '../helpers/response';
 
 const validator = new Validator();
 
@@ -8,6 +9,7 @@ export const validateRequestBody = (schema) => (req, res, next) => {
   if (errors.length === 0) {
     next();
   } else {
-    res.status(400).send(errors.map((error) => error.stack));
+    const error = { message: errors.map((error) => error.stack).join(', ') };
+    res.status(400).send(setErrorResponse(error, 400));
   }
 };
