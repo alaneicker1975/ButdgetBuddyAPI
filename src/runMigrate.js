267dd450e865d86b3ba67b9f1671c82d6091af5d
Alan Eicker <alaneicker@gmail.com>
2023-04-13 20:41:32 -0500

    refactors runMigrate.js

--- src/runMigrate.js
+++ src/runMigrate.js
@@ -3,18 +3,19 @@ require('dotenv').config();
 
 const { DB, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
-const dbConfig = {
-  database: DB,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  port: +DB_PORT,
-  ensureDatabaseExists: true,
-};
-
 (async () => {
   try {
-    await migrate(dbConfig, './src/migrations');
+    await migrate(
+      {
+        database: DB,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        host: DB_HOST,
+        port: +DB_PORT,
+        ensureDatabaseExists: true,
+      },
+      './src/migrations',
+    );
     console.log('Migration completed');
   } catch (e) {
     console.log('Could not run migration: ', e.message);
