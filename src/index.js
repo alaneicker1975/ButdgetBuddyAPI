740cd09ccc461652f6119e5aa674b0ab296406fe
Alan Eicker <alaneicker@gmail.com>
2023-04-16 15:17:48 -0500

    Refactors routes import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import './db';
-import { expense } from './routes';
+import * as routes from './routes';
 
 const app = express();
 const port = process.env.PORT || 9000;
@@ -11,6 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use(`${process.env.BASE_URL}/expense`, expense);
+app.use(`${process.env.BASE_URL}/expense`, routes.expense);
 
 app.listen(port, () => console.log('Server running on port', port));
