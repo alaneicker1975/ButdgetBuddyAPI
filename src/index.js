42332d50596d3786105c44c7c2eee0539fa1754a
Alan Eicker <alaneicker@gmail.com>
2023-05-05 10:29:36 -0500

    Adds swagger docs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 import './db';
 import * as routes from './routes';
@@ -13,6 +15,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 
+app.use(
+  `${process.env.BASE_URL}/api-docs`,
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocument),
+);
 app.use(`${process.env.BASE_URL}/expense`, routes.expense);
 
 app.listen(port, () => console.log('Server running on port', port));
