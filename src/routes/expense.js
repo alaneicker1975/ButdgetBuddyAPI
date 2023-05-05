1d693dd016404e8b7c593ecd52be692d398228c2
Alan Eicker <alaneicker@gmail.com>
2023-05-05 09:11:04 -0500

    Adds request body validation to expense POST request

--- src/routes/expense.js
+++ src/routes/expense.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import * as expense from '../controllers/expense';
+import { validateRequestBody } from '../middleware/validateRequestBody';
+import { expenseSchema } from '../schemas/expenses';
 
 const router = Router();
 
 router.get('/', expense.getAll);
 router.get('/:id', expense.getOne);
-router.post('/', expense.insertOne);
+router.post('/', validateRequestBody(expenseSchema), expense.insertOne);
 router.patch('/:id', expense.updateOne);
 router.delete('/:id', expense.deleteOne);
 
