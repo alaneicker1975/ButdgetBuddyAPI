086bb256f02422fa6d016b89db75afa03ce3e3f5
Alan Eicker <alaneicker@gmail.com>
2023-05-07 15:38:06 -0500

    removes migration

--- migrations/2_adds-user.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO
-  user_account (user_account_id, username, password, email)
-VALUES
-  (
-    '973ad0cc-8709-4913-b6ad-43db8645e533',
-    'billybob',
-    'rEK1ecacw.7.c',
-    'bb@gmail.com'
-  );
