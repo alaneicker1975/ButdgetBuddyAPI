04859ad41583a584fdc669e57ba34f74e570c24a
Alan Eicker <alaneicker@gmail.com>
2023-05-08 09:05:25 -0500

    user auth workflow

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,6 +2,25 @@ import jwt from 'jsonwebtoken';
 import { setErrorResponse } from '../helpers/response';
 import * as auth from '../models/auth';
 
-export const authenticateUser = (req, res) => {
-  const { username, password } = req.body;
+const loginError = 'Username or password is incorrect';
+
+export const authenticateUser = async (req, res) => {
+  try {
+    const { user, error } = await auth.authenticateUser(req.body);
+    const status = error?.status || 200;
+
+    res.status(status).send(
+      error
+        ? setErrorResponse(error, status)
+        : {
+            data: {
+              token: jwt.sign(user, process.env.JWT_SECRET, {
+                expiresIn: 1800,
+              }),
+            },
+          },
+    );
+  } catch (error) {
+    res.status(500).send(setErrorResponse({ message: loginError }, 500));
+  }
 };
--- src/models/auth.js
+++ src/models/auth.js
@@ -1,2 +1,4 @@
 import { pool } from '../db';
 import { setUpdatePlaceholders, getValues } from '../helpers/query';
+
+export const authenticateUser = ({ username, password }) => {};
