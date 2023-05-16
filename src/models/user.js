f27a4727992d405076dd5ef66b9341ccbce131e6
Alan Eicker <alaneicker@gmail.com>
2023-05-15 21:09:08 -0500

    updates user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,14 @@
 import { pool } from '../db';
 import { setUpdatePlaceholders, getValues } from '../helpers/query';
 
-// User models
+export const createUser = async (req, res, next) => {
+  // Inserts a user
+};
+
+export const updateUser = async (req, res, next) => {
+  // Updates a user
+};
+
+export const deleteUser = async (req, res, next) => {
+  // Deletes a user
+};
