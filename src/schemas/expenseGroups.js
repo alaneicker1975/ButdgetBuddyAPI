6df1da1ac22bb3f3dea285a39afd7a6d10b37fe6
Alan Eicker <alaneicker@gmail.com>
2023-06-16 12:32:03 -0500

    refactors schemas

--- src/schemas/expenseGroups.js
+++ src/schemas/expenseGroups.js
@@ -1,7 +1,6 @@
 import Joi from 'joi';
 
 export const expenseGroupSchema = Joi.object({
-  expense_group_id: Joi.number(),
   user_account_id: Joi.number().required(),
   start_date: Joi.string().required(),
   end_date: Joi.string().required(),
@@ -9,6 +8,5 @@ export const expenseGroupSchema = Joi.object({
 });
 
 export const expenseGroupExpenseSchema = Joi.object({
-  expense_group_id: Joi.number(),
   name: Joi.string().max(50).required(),
 });
--- src/schemas/expenses.js
+++ src/schemas/expenses.js
@@ -1,6 +1,5 @@
 import Joi from 'joi';
 
 export const expenseSchema = Joi.object({
-  expense_id: Joi.number(),
   name: Joi.string().max(50).required(),
 });
